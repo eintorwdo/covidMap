4e84f155b7b153a7a81df0ff24f1d9a39fbf2e6a
unknown <48072394+eintorwdo@users.noreply.github.com>
2020-12-02 06:53:43 +0100

    Components/Navbar: added search input with autocomplete. Does not do anything for now

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,6 +5,30 @@ import style from './style.module.css';
 export default function Navbar(props){
     const {country, countryClicked, countryNames} = useContext(ModeContext);
     const [countryName, setCountryName] = useState(null);
+    const [dataList, setDataList] = useState(null);
+    const dataListRef = useRef(null);
+
+    const onImputChange = (e) => {
+        if(e.target.value.length > 0){
+            const hints = [];
+            let options;
+
+            for(let el of countryNames){
+                if(el.toLowerCase().startsWith(e.target.value.toLowerCase())){
+                    hints.push(el);
+                }
+            }
+
+            setDataList(hints);
+        }
+        else{
+            setDataList([]);
+        }
+    }
+
+    useEffect(() => {
+        console.log(dataList)
+    }, [dataList])
 
     useEffect(() => {
         let div;
@@ -32,6 +56,12 @@ export default function Navbar(props){
             <div className={`${style.navbar} ${props.sticky ? style.sticky : ''}`}>
                 <h1 className="header">COVID-19 dashboard</h1>
                 <div className={style['navbar-bottom']}>
+                    <input className={style['input-search']} autoComplete="off" type="text" name="country" list="dataList" placeholder="Search by country" onChange={onImputChange}/>
+                    <datalist id="dataList">
+                        {dataList?.map((el, i) => {
+                            return <option key={i} value={el}></option>
+                        })}
+                    </datalist>
                     {countryName}
                     <a href="#chart-wrapper" className={`${style['button-link']}`}>
                         <div className={`${style['graphs-button']}`}>
